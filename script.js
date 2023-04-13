c46b0b6a95a0b299c6a861c04c3f181f4b9772f7
Alice <aliceli083@gmail.com>
2023-04-13 15:40:14 -0700

    question index

--- script.js
+++ script.js
@@ -15,24 +15,24 @@ var saveBtn = document.getElementById("saveBtn");
 var totalTime = document.getElementById("totalTime");
 var questions = [
     {
-        question: "add later",
-        choices: ["add later","more", "stuff"],
-        answer: "add later"
+        question: "what is 10 + 10?",
+        choices: ["10", "20", "30", "40"],
+        answer: "20"
     },
     {
-        question: "add later",
-        choices: ["add later","more", "stuff"],
-        answer: "add later"
+        question: "How many legs are on a spider?",
+        choices: ["2", "4", "6", "8"],
+        answer: "8"
     },
     {
-        question: "add later",
-        choices: ["add later" ,"more", "stuff"],
-        answer: "add later"
+        question: "Is JavaScript confusing?",
+        choices: ["no", "kind of", "a little", "yes"],
+        answer: "yes"
     },
     {
-        question: "add later",
-        choices: ["add later","more", "stuff"],
-        answer: "add later"
+        question: "Was this assignment hard?",
+        choices: ["yes", "no", "very time consuming", "very hard"],
+        answer: "very hard"
     }
 ];
 var timerID;
@@ -46,44 +46,51 @@ function countdown() {
     timeLeft--
     if (timeLeft < 0) {
         timeLeft = 0;
-        clearInterval (timerID);
+        clearInterval(timerID);
     }
-    
+
     totalTime.textContent = timeLeft;
-   
+
 }
 
 
 function displayQuestion() {
-    questionEl.textContent = questions[index].question;
-    
+    questionEl.textContent = questions[questionIndex].question;
+
     choicesEl.innterHTML = "";
-    questions[index].choices.forEach(function (choices) {
-        var choiceBtn = document.createElement("checkbox");
-        var brk= document.createElement("br");
-        choiceBtn.textContent= choices;
+    questions[questionIndex].choices.forEach(function (choices) {
+        var choiceBtn = document.createElement("button");
+        var brk = document.createElement("br");
+        choiceBtn.textContent = choices;
         choiceBtn.addEventListener("click", checkAnswer);
         choicesEl.appendChild(choiceBtn);
         choicesEl.appendChild(brk);
     })
 };
 
+
 function checkAnswer(event) {
-    var selectedChoice = event.textContent;
-    var Answer = questions.answer;
+    var selectedChoice = event.target.textContent;
+    var Answer = questions[questionIndex].answer;
     if (selectedChoice === Answer) {
         resultsEl.textContent = "Correct";
     }
     else {
         resultsEl.textContent = "Incorrect";
-        timeLeft = -10;
+        timeLeft -= 10;
         if (timeLeft < 0) {
             timeLeft = 0;
-            clearInterval (timerID);
+            clearInterval(timerID);
         }
     }
 }
 
 startBtn.addEventListener("click", startQuiz);
 
-
+// questionIndex++
+// if (questionIndex < questions.length) {
+//    showQuestion();
+// }
+// else {
+//     endQuiz();
+// }
